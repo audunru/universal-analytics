a975a3166a7ef26092cc2f9565b5ecc8138032cd
Audun Rundberg <audun@rundberg.no>
2019-10-07 10:35:04 +0200

    Cookie domain can be set in constructor #1

--- class.universalAnalyticsCookie.php
+++ class.universalAnalyticsCookie.php
@@ -10,10 +10,28 @@ class universalAnalyticsCookie
     * Modified by Audun Rundberg
     */
 
-    /*
-    * Get cid from the cookie
-    */
+    /**
+     * Hostname to use when setting the cookie.
+     *
+     * @var string|null
+     */
+    private $cookieDomain;
 
+    /**
+     * Create a new cookie instance.
+     *
+     * @param string|null $cookieDomain
+     */
+    public function __construct($cookieDomain = null)
+    {
+        $this->cookieDomain = $cookieDomain;
+    }
+
+    /**
+     * Get cid from the cookie.
+     *
+     * @return string|bool
+     */
     public function getCid()
     {
         if (isset($_COOKIE['_ga'])) {
@@ -25,12 +43,13 @@ class universalAnalyticsCookie
         }
     }
 
-    /*
-    * Handle the parsing of the _ga cookie
-    * This assumes the 32bit + 32bit CID format, not the new
-    * UUID v4 format
-    */
-
+    /**
+     * Handle the parsing of the _ga cookie
+     * This assumes the 32bit + 32bit CID format, not the new
+     * UUID v4 format.
+     *
+     * @return array
+     */
     private function parse()
     {
         list($version, $domainDepth, $cid1, $cid2) = explode('.', $_COOKIE['_ga'], 4);
@@ -38,17 +57,18 @@ class universalAnalyticsCookie
         return ['version' => $version, 'domainDepth' => $domainDepth, 'cid' => $cid1.'.'.$cid2];
     }
 
-    /*
-    * Creates a new GA cookie and returns the tracking id (cid)
-    */
-
+    /**
+     * Creates a new GA cookie and returns the tracking id (cid).
+     *
+     * @return string
+     */
     public function set()
     {
         $cid1 = mt_rand(0, 2147483647);
         $cid2 = mt_rand(0, 2147483647);
 
         $cid = $cid1.'.'.$cid2;
-        setcookie('_ga', '1.2.'.$cid, time() + 60 * 60 * 24 * 365 * 2, '/');
+        setcookie('_ga', '1.2.'.$cid, time() + 60 * 60 * 24 * 365 * 2, '/', $this->cookieDomain);
 
         return $cid;
     }
